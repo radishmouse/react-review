4eb8009367d8c66eaef2894ddbf3a56d24b3be6b
Chris Aquino <chrisaquino@radishmouse.local>
2020-02-03 10:18:12 -0500

    adds Greet component example

---
+++ src/Greet.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+Greet.defaultProps = {
+    color: 'green'
+}
+
+function Greet({
+    whom, 
+    how,
+    color
+}) {
+    return (
+        <div 
+            style={{
+                color
+            }}
+            className="card greet"
+        >
+            <h1>{how}, {whom}!</h1>
+        </div>
+    );
+}
+
+export default Greet;
